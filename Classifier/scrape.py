4c61e5a72a29a6904b2b0e2b91c8a7132bdba846
Lucas Sylvester <lucassylvester02@gmail.com>
2023-09-30 16:02:59 -0400

    idk man so many things are happening

--- Classifier/scrape.py
+++ Classifier/scrape.py
@@ -2,37 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+def get_articles_dataset(path, limit=500, start=0):
+    articles = []
+    with open(path, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        csv_header = 0
+        for row in reader:
+            if csv_header == 0:
+                csv_header = 1
+                continue
+            elif csv_header < start:
+                csv_header += 1
+                continue
+            if csv_header == limit:
+                break
+            url = row[3]
+            article = row[0]
+            response = requests.get(str(url))
+            if response.status_code == 200:
+                # Parse the HTML content with BeautifulSoup
+                soup = BeautifulSoup(response.text, 'html.parser')
+                
+                # Find all <p> tags in the HTML
+                p_tags = soup.find_all('p')
+                
+                # Loop through and print the text within each <p> tag
+                for p_tag in p_tags:
+                    article += p_tag.get_text() # Get the text within the <p> tag
+                articles.append(article)  # Get the text within the <p> tag
+                # print(article)
+            else:
+                print("Failed to retrieve the HTML content.")
+            print(csv_header)
+            csv_header += 1
+    return articles
 
-with open('./Classifier/datasets/code_articles.csv', 'r', newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    i = 0
-    for row in reader:
-        if i == 0:
-            i += 1
-            continue
-        url = row[3]
-        response = requests.get(str(url))
-        if response.status_code == 200:
-            # Parse the HTML content with BeautifulSoup
-            soup = BeautifulSoup(response.text, 'html.parser')
-            
-            # Find all <p> tags in the HTML
-            p_tags = soup.find_all('p')
-            
-            # Loop through and print the text within each <p> tag
-            for p_tag in p_tags:
-                print(p_tag.get_text())  # Get the text within the <p> tag
-        else:
-            print("Failed to retrieve the HTML content.")
-        
-# html = r.text
-
-# #soup = BeautifulSoup(html, "html5lib")
-# soup = BeautifulSoup(html, "lxml")
-# print(soup.title)
-# print(soup.get_text())
-
-
+# print(get_articles_dataset('Classifier/datasets/code_articles.csv'))
 
 
 
--- api/main.py
+++ api/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-#uvicorn ./api/main:app --reload
+#uvicorn main:app --reload
 @app.get("/classify/{item_id}")
 async def read_user_item(item_id: str):
     print(item_id)
     return item_id
+
+#uvicorn main:app --reload
+@app.post("/post")
+async def read_post(data: str):
+    print(data)
+    return data
 
 @app.get("/hello")
 async def root():
